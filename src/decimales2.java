533551ad1dc67c5dc1e043e352ed31b17aff7af8
Jose Valenzuela <valenz743@gmail.com>
2022-06-28 13:42:05 -0500

    Este es mi primer commit de este proyecto

---
+++ src/decimales2.java
@@ -0,0 +1,10 @@
+class Decimales2 {
+    public static void main(String[] args) {
+        float a = 10.5f;
+        float b = 1.5f;
+        float c = a + b;
+
+        System.out.println(c);
+
+    }
+}
